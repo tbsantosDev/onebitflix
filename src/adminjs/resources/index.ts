5fe4ca4af41e0fa623be73ead4cf5269023122e5
Thiago <dev.tbsantos@gmail.com>
2024-02-09 15:42:55 -0300

    Upload de Arquivos no AdminJs

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,7 +1,8 @@
 import { ResourceWithOptions } from "adminjs";
 import { categoryResourceOptions } from "./category";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { courseResourceOptions } from "./course";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
@@ -11,5 +12,10 @@ export const adminJsResources: ResourceWithOptions[] = [
     {
         resource: Course,
         options: courseResourceOptions
+    },
+    {
+        resource: Episode,
+        options: episodeResourceOptions,
+        features: episodeResourceFeatures
     }
 ]
