18d33afb9faa53f6c85097e8ff582c18395017a5
Thiago <dev.tbsantos@gmail.com>
2024-02-27 15:19:58 -0300

    Obtendo informações da conta de usuario

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,17 +3,29 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
-    //GET /users/current/watching
-    watching: async (req: AuthenticatedRequest, res: Response) => {
-        const { id } = req.user!
-
-        try {
-            const watching = await userService.getKeepWatchingList(id)
-            return res.json(watching)
-        } catch (err) {
-            if (err instanceof Error) {
-                return res.status(400).json({ message: err.message });
-              }
-        }
+  //GET /users/current
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    try {
+      const currentUser = req.user!;
+      return res.json(currentUser);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
     }
-}
+  },
+
+  //GET /users/current/watching
+  watching: async (req: AuthenticatedRequest, res: Response) => {
+    const { id } = req.user!;
+
+    try {
+      const watching = await userService.getKeepWatchingList(id);
+      return res.json(watching);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+};
