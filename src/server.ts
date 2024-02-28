0dc93d9e129ee4fe9741ca669c5969e8b2f23ee7
Thiago <dev.tbsantos@gmail.com>
2024-02-28 13:35:22 -0300

    Adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors'
 import { sequelize } from './database';
 import { adminJSRouter, adminJs } from './adminjs';
 import { router } from './routes'
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
 
 app.use(express.static('public'))
+
 //app.use(caminho, rotas)
 app.use(adminJs.options.rootPath, adminJSRouter)
 app.use(router)
