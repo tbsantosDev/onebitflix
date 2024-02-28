c6536b3eff67c256b7ddea8cd4441a141b92ef52
Thiago <dev.tbsantos@gmail.com>
2024-02-28 13:24:46 -0300

    Atualizando a senha

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -17,17 +17,44 @@ export const usersController = {
 
   //PUT /users/current
   update: async (req: AuthenticatedRequest, res: Response) => {
-    const { id } = req.user!
-    const { firstName, lastName, phone, email, birth } = req.body
+    const { id } = req.user!;
+    const { firstName, lastName, phone, email, birth } = req.body;
 
     try {
-      const updatedUser = await userService.update(id, {firstName, lastName, phone, email, birth})
-      return res.json(updatedUser)
+      const updatedUser = await userService.update(id, {
+        firstName,
+        lastName,
+        phone,
+        email,
+        birth,
+      });
+      return res.json(updatedUser);
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message });
       }
     }
+  },
+
+  //PUT /users/current/password
+  updatePassword: async (req: AuthenticatedRequest, res: Response) => {
+    const user = req.user!;
+    const { currentPassword, newPassword } = req.body;
+
+    user.checkPassword(currentPassword, async (err, isSame) => {
+      try {
+        if (err) throw new Error("Erro de verificação");
+        if (!isSame) throw new Error("Senha incorreta");
+
+        await userService.updatePassword(user.id, newPassword);
+
+        return res.status(204).send();
+      } catch (err) {
+        if (err instanceof Error) {
+          return res.status(400).json({ message: err.message });
+        }
+      }
+    });
   },
 
   //GET /users/current/watching
